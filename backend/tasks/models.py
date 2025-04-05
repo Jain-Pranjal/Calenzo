6ae5caf2edd6c0e5a36453744c4a6c693ac8e84c
Pranjal Jain <pranjalworkon@gmail.com>
2025-04-05 23:37:49 +0530

    feat: build model and viewset of task

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+PRIORITY_CHOICES = [
+    (0, "Low"),
+    (1, "Medium"),
+    (2, "High"),
+]
+
+STATUS_CHOICES = [
+    ("todo", "To Do"),
+    ("inprogress", "In Progress"),
+    ("done", "Done"),
+]
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, blank=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default="todo")
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
+    
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # neeed to make sure that only the authenticated user can see their own tasks
+    def get_queryset(self):
+        queryset = self.queryset
+        user = self.request.user
+        if user.is_authenticated:
+            queryset = queryset.filter(user=user)
+        return queryset
+    
+    # need to make sure that only the authenticated user can create their own tasks
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# get_queryset aur perform_create sirf internal logic ko control karte hain, URL path mein dikhte nahi.
+
